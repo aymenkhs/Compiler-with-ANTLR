0474b0194a7a517ce07f334ee43b2bd63d032712
aymmenkhs <aymenkhouas@gmail.com>
2021-03-18 20:16:32 +0100

    Quadruplets: basic structure

--- src/Quadruplets.java
+++ src/Quadruplets.java
@@ -1,5 +1,25 @@
 import java.util.ArrayList;
 
 public class Quadruplets {
-    private ArrayList<QuadElement> quadruplets = new ArrayList<QuadElement>();
+    private ArrayList<QuadElement> quadruplets;
+
+    public Quadruplets(){
+        this.quadruplets = new ArrayList<QuadElement>();
+    }
+
+    public void addQuad(QuadElement quad){
+        this.quadruplets.add(quad);
+    }
+
+    public void addQuad(String Elem1, String Elem2, String Elem3, String Elem4){
+        this.quadruplets.add(new QuadElement(Elem1, Elem2, Elem3, Elem4));
+    }
+
+    public QuadElement getQuad(int index){
+        return this.quadruplets.get(index);
+    }
+
+    public int size(){
+        return this.quadruplets.size();
+    }
 }
