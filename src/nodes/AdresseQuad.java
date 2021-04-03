9e532f7e97fc691cae2fe7e3501d022ca016ff5e
aymmenkhs <aymenkhouas@gmail.com>
2021-04-03 18:24:06 +0100

    adding 2 nodes (adresse and bool)

---
+++ src/nodes/AdresseQuad.java
@@ -0,0 +1,10 @@
+package nodes;
+
+public class AdresseQuad extends Node{
+
+    private int adresse;
+
+    public AdresseQuad(int adresse) {
+        this.adresse = adresse;
+    }
+}
---
+++ src/nodes/ConstanteBoolean.java
@@ -0,0 +1,15 @@
+package nodes;
+
+public class ConstanteBoolean extends Constante{
+
+    private boolean value;
+
+    public ConstanteBoolean(String type, boolean value) {
+        super(type);
+        this.value = value;
+    }
+
+    public Boolean getValue() {
+        return value;
+    }
+}
